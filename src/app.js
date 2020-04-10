0ffc81e471d6887951ad5477392e0bdac5c9e588
Dmitry Osipenko <eympel@gmail.com>
2020-04-10 20:46:41 +0300

    feat: added block movement by mouse click, timer operation and number of steps

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
-import puzzle from './module/class.js';
-let size = '4';
-let gemPuzzle = new puzzle(size);
+import puzzle from './module/class.js'
 
-gemPuzzle.createPazzle();
-gemPuzzle.renderPuzzle();
-gemPuzzle.addListener()
+window.onload = () => {
+    let size = '4'
+    let gemPuzzle = new puzzle(size)
+
+    gemPuzzle.createPazzle()
+    gemPuzzle.renderPuzzle()
+    gemPuzzle.addListener()
+}
--- src/module/class.js
+++ src/module/class.js
@@ -1,10 +1,16 @@
 export default class puzzle {
+    static answer = ''
+    // static step = 0
+    static minSpan
+    static secSpan 
+    static mainContainerWrapperPuzzle
+    static stepSpan = '0'//document.querySelector('#wrapper_info__steps')
     constructor(size) {
       this.size = size
     }
 
     createPazzle() {
-        let mainContainer = puzzle.createElem('div', [], 'mainContainer')
+        let mainContainer = puzzle.createElem('div', [], 'main_container')
         let mainContainerSettings = puzzle.createElem('div', [], 'setting')
         mainContainerSettings.innerHTML = `<input class='settingButton' name='settingStart' type='button' value='Размешать и начать'>`
         mainContainerSettings.innerHTML += `<input class='settingButton' name='settingStop' type='button' value='Стоп'>`
@@ -13,9 +19,9 @@ export default class puzzle {
 
         let mainContainerWrapper = puzzle.createElem('div', [], 'wrapper')
         let mainContainerWrapperInfo = puzzle.createElem('div', [], 'wrapper_info')
-        mainContainerWrapperInfo.innerHTML = '<p>Ходов: <span id="wrapper_info__steps">12</span> Время: <span id="wrapper_info__time">12:43</span></p>'
-        this.mainContainerWrapperPuzzle = puzzle.createElem('div', [], 'wrapper_puzzle')
-        mainContainerWrapper.append(mainContainerWrapperInfo, this.mainContainerWrapperPuzzle)
+        mainContainerWrapperInfo.innerHTML = `<p>Ходов: <span id="wrapper_info__steps">0</span> Время: <span id="wrapper_info__time"><span id="time_min">00</span>:<span id ="time_sec">00</span></span></p>`
+        puzzle.mainContainerWrapperPuzzle = puzzle.createElem('div', [], 'wrapper_puzzle')
+        mainContainerWrapper.append(mainContainerWrapperInfo, puzzle.mainContainerWrapperPuzzle)
 
         let mainContainerPuzzleSize = puzzle.createElem('div', [], 'puzzleSize')
         mainContainerPuzzleSize.innerHTML = `<p>Размер поля ${this.size}x${this.size}</p>`;
@@ -32,34 +38,45 @@ export default class puzzle {
     }
 
     renderPuzzle(num = null) {
+        puzzle.minSpan = document.querySelector('#time_min')
+        puzzle.secSpan = document.querySelector('#time_sec')
+        puzzle.answer = ''
+        puzzle.stepSpan = document.querySelector('#wrapper_info__steps')
+        puzzle.stepSpan.innerText = '0'
+
         let sizePuzzle = num === null ? this.size : num;
         let size = Math.pow(sizePuzzle, 2)
         for (let i = 1; i <= size - 1; i++) {
             let puzzleContainer = puzzle.createElem('div', [], 'wrapper_puzzle__container')
             puzzleContainer.classList.add(`col_${sizePuzzle}`)
             puzzleContainer.innerHTML = `<div class="container" id="container_${i}"><p class="wrapper_puzzle__container-text">${i}</p></div>`;
-            this.mainContainerWrapperPuzzle.append(puzzleContainer)
+            puzzle.mainContainerWrapperPuzzle.append(puzzleContainer)
+            puzzle.answer += i
         }
-        let lastPuzzleContainer = puzzle.createElem('div', [['id', `container_${size}`]], 'wrapper_puzzle__container')
+        puzzle.answer += size
+        let lastPuzzleContainer = puzzle.createElem('div', [['id', `last_container`]], 'wrapper_puzzle__container')
+        lastPuzzleContainer.innerHTML = `<div id="container_${size}"><p class="wrapper_puzzle__container-last">${size}</p></div>`;
         lastPuzzleContainer.classList.add(`col_${sizePuzzle}`)
-        this.mainContainerWrapperPuzzle.append(lastPuzzleContainer)
+        puzzle.mainContainerWrapperPuzzle.append(lastPuzzleContainer)
+        document.querySelector('.puzzleSize p').innerHTML = `Размер поля ${sizePuzzle}x${sizePuzzle}`
     }
 
     addListener() {
         document.querySelector('.puzzleSize').addEventListener('click', (e) => {
+        document.querySelector('.wrapper_puzzle').removeEventListener('mouseup', puzzle.mouseUp, false)
+        clearInterval(puzzle.timer)
+        puzzle.minSpan.innerText = '00'
+        puzzle.secSpan.innerText = '00'
             if (e.target.classList.contains('size_puzzle')) {
                 document.querySelectorAll('.wrapper_puzzle__container').forEach(container => {
                     container.remove();
                 })
                 this.renderPuzzle(e.target.innerText[0])
             }
-            // e.target.style = 'cursor: not-allowed;'
-            console.log(this.mainContainerWrapperPuzzle)
+            // e.target.style = 'cursor: not-allowed;' 
         })
-        
+        document.querySelector('.setting').addEventListener('click', puzzle.installSettings, false)
     }
-
-
 
     static createElem(element, atributes = [], ...classes) {
         let elem = document.createElement(element)
@@ -70,4 +87,138 @@ export default class puzzle {
         return elem
     }
 
+    static mouseUp (e) {
+        let selectedContainer = e.target.closest('div')
+ 
+        if (selectedContainer.classList.contains('container')) {
+
+            let emptyContainer = document.querySelector('#last_container')
+            let topSelectedContainer = selectedContainer.getBoundingClientRect().top
+            let topEmptyContainer = emptyContainer.getBoundingClientRect().top
+            let leftSelectedContainer = selectedContainer.getBoundingClientRect().left
+            let leftEmptyContainer = emptyContainer.getBoundingClientRect().left
+            let widthContainer = selectedContainer.getBoundingClientRect().width
+
+            if (leftSelectedContainer === leftEmptyContainer) {
+                if (Math.floor(topEmptyContainer - topSelectedContainer)  === Math.floor(widthContainer)) { 
+                    this.removeEventListener('mouseup', puzzle.mouseUp, false)
+                    puzzle.moveContainer(selectedContainer, emptyContainer, 'top')
+                }
+                if (Math.floor(topSelectedContainer - topEmptyContainer)  === Math.floor(widthContainer)) { 
+                    this.removeEventListener('mouseup', puzzle.mouseUp, false)
+                    puzzle.moveContainer(selectedContainer, emptyContainer, 'bottom')
+                }
+            }
+            if (topSelectedContainer === topEmptyContainer) {
+                if (Math.floor(leftEmptyContainer - leftSelectedContainer)  === Math.floor(widthContainer)) { 
+                    this.removeEventListener('mouseup', puzzle.mouseUp, false)
+                    puzzle.moveContainer(selectedContainer, emptyContainer, 'left')
+                }
+                if (Math.floor(leftSelectedContainer - leftEmptyContainer) === Math.floor(widthContainer)) { 
+                    this.removeEventListener('mouseup', puzzle.mouseUp, false)
+                    puzzle.moveContainer(selectedContainer, emptyContainer, 'right')
+                }
+            }
+            
+            
+        }
+    }
+
+    static moveContainer(container, emptyContainer, direction) {
+        let start = Date.now()
+        let timer = setInterval(() => {
+            let timePassed = Date.now() - start;
+            if (timePassed >= 500) {
+                clearInterval(timer)
+                return
+            }
+            container.style = `z-index: 1000; ${direction}: ${timePassed/4}px`;
+
+        let promise = new Promise((resolve, reject) => {
+            let result;
+            if (direction === 'top') {
+                if (emptyContainer.getBoundingClientRect().top <= container.getBoundingClientRect().top) {
+                    result = puzzle.checkPositionActiveContainer(container, emptyContainer, timer)
+                    }
+            }
+            if (direction === 'bottom') {
+                if (Math.floor(emptyContainer.getBoundingClientRect().top) >= Math.floor(container.getBoundingClientRect().top)) {
+                    result = puzzle.checkPositionActiveContainer(container, emptyContainer, timer)
+                    }
+            }
+            if (direction === 'left') {
+                if (emptyContainer.getBoundingClientRect().left <= container.getBoundingClientRect().left) {
+                    result = puzzle.checkPositionActiveContainer(container, emptyContainer, timer)
+                    }
+            }
+            if (direction === 'right') {
+                if (emptyContainer.getBoundingClientRect().left >= container.getBoundingClientRect().left) {
+                    result = puzzle.checkPositionActiveContainer(container, emptyContainer, timer)
+                    }
+            }
+            setTimeout(() => resolve(result), 20);
+        });
+            let resAnswer = '';
+            promise.then(result => {
+                if (result === true) {
+                    document.querySelectorAll('.wrapper_puzzle__container p').forEach(res => {
+                        resAnswer += res.innerText
+                    })
+                    if (this.answer === resAnswer) alert('you are the best !')
+                }
+            })
+        })
+    }
+
+    static checkPositionActiveContainer(container, emptyContainer, timer) {
+        puzzle.stepSpan.innerText = Number(puzzle.stepSpan.innerText) + 1
+        let numEmptyContainer = emptyContainer.childNodes[0]
+        emptyContainer.removeAttribute('id')
+        emptyContainer.removeAttribute('style')
+        container.removeAttribute('style')
+        container.parentNode.setAttribute('id', 'last_container')
+        container.parentNode.append(numEmptyContainer)
+        emptyContainer.append(container)
+        clearInterval(timer)
+        document.querySelector('.wrapper_puzzle').addEventListener('mouseup', puzzle.mouseUp, false)
+        return true
+    }
+
+    static installSettings(e) {
+        if (e.target.getAttribute('name') === 'settingStart') {
+            document.querySelector('.wrapper_puzzle').addEventListener('mouseup', puzzle.mouseUp, false)
+            // console.log(e.target)
+            clearInterval(puzzle.timer)
+            puzzle.stepSpan.innerText = '0'
+            let arrauDiv = []
+            document.querySelectorAll('.wrapper_puzzle__container ').forEach(elem => {
+                arrauDiv.push(elem)
+                elem.remove()
+            })
+            arrauDiv.sort(() => Math.random() - 0.5);
+            arrauDiv.forEach(elem => {
+                puzzle.mainContainerWrapperPuzzle.append(elem)
+            })
+            puzzle.minSpan.innerText = '00'
+            puzzle.secSpan.innerText = '00'
+            let min = 0
+            let sec = 0
+            puzzle.timer = setInterval(() => {
+                sec += 1
+                if (sec < 10) puzzle.secSpan.innerText = `0${sec}`
+                else if (sec >= 10 && sec < 60) puzzle.secSpan.innerText = sec
+                if (sec === 60) {
+                    puzzle.secSpan.innerText = '00'
+                    min += 1
+                    sec = 0
+                    if (min < 10) puzzle.minSpan.innerText = `0${min}`
+                    else if (min >= 10 && min < 60) puzzle.minSpan.innerText = min
+                }
+            }, 1000)
+        }
+        if (e.target.getAttribute('name') === 'settingStop') {
+            clearInterval(puzzle.timer)
+            document.querySelector('.wrapper_puzzle').removeEventListener('mouseup', puzzle.mouseUp, false)
+        }
+    }
 }
